a4dc478c285f285aec5350dfba5a0d51dec48e14
Manuel Schaich <rmschaich@outlook.com>
2023-11-19 16:14:49 +0000

    Update existing code

--- src/facetEnumeration.cpp
+++ src/facetEnumeration.cpp
@@ -1,57 +1,44 @@
-#include "eigenConversion.hpp"
-#include "mexFunctions.hpp"
+#include "mex.hpp"
+#include "mexAdapter.hpp"
+#include "eigen/conversions.hpp"
 #include "lrsinterface.hpp"
 
 FILE *lrs_ifp;
 FILE *lrs_ofp;
 
-typedef matlab::data::Array mexArray;
-
 class MexFunction : public matlab::mex::Function {
-private:
-    std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr;
-    ArrayFactory factory;
-    void displayError(std::string errorMessage) {
-        matlabPtr->feval(
-                        matlab::engine::convertUTF8StringToUTF16String("error"),
-                        0, 
-                        std::vector<mexArray>({
-                            factory.createScalar(errorMessage) 
-                            })
-                        );
-    }
 
 public:
-    MexFunction() : matlabPtr(getEngine()) {}
-    ~MexFunction() {}
-    void operator()(ArgumentList outputs, ArgumentList inputs) {
+    MexFunction() {
+        matlabPtr = getEngine();
+    }
+    ~MexFunction() = default;
+    void operator()(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs) {
         
         if (inputs.size() < 1)
-            displayError("At least vertices have to be supplied");
+            utilities::error("At least vertices have to be supplied");
         
-        if (!isnumeric(inputs[0]))
-            displayError("Vertices have to be numeric.");
-
-        
+        if (!utilities::isnumeric(inputs[0]))
+            utilities::error("Vertices have to be numeric.");
 
         Polytope poly;
 
         if (inputs.size()>1) {
             if (inputs[0].getDimensions()[1] != inputs[1].getDimensions()[1])
-                displayError("Vertices and rays have to be in same dimension");
+                utilities::error("Vertices and rays have to be in same dimension");
             
-            poly.setRays(fromMatrixXd(convert(inputs[1])));
+            poly.setRays(fromMatrixXd(utilities::eigen::convert(inputs[1])));
         }
 
         
-        poly.setVertices(fromMatrixXd(convert(inputs[0])));
+        poly.setVertices(fromMatrixXd(utilities::eigen::convert(inputs[0])));
 
 
         if (outputs.size()>0)
-            outputs[0] = convert(factory, fromMatrixXq(poly.getA()));
+            outputs[0] = utilities::eigen::convert(fromMatrixXq(poly.getA()));
 
         if (outputs.size()>1)
-            outputs[1] = convert(factory, fromMatrixXq(poly.getB()));
+            outputs[1] = utilities::eigen::convert(fromMatrixXq(poly.getB()));
 
     }
 };
--- src/vertexEnumeration.cpp
+++ src/vertexEnumeration.cpp
@@ -1,60 +1,54 @@
-#include "eigenConversion.hpp"
-#include "mexFunctions.hpp"
+#include "mex.hpp"
+#include "mexAdapter.hpp"
+#include "eigen/conversions.hpp"
 #include "lrsinterface.hpp"
 
 FILE *lrs_ifp;
 FILE *lrs_ofp;
 
-typedef matlab::data::Array mexArray;
-
-class MexFunction : public matlab::mex::Function {
-private:
-    std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr;
-    ArrayFactory factory;
-    void displayError(std::string errorMessage) {
-        matlabPtr->feval(
-                        matlab::engine::convertUTF8StringToUTF16String("error"),
-                        0, 
-                        std::vector<mexArray>({
-                            factory.createScalar(errorMessage) 
-                            })
-                        );
-    }
+class MexFunction : public matlab::mex::Function
+{
 
 public:
-    MexFunction() : matlabPtr(getEngine()) {}
-    ~MexFunction() {}
-    void operator()(ArgumentList outputs, ArgumentList inputs) {
-        
+    MexFunction()
+    {
+        matlabPtr = getEngine();
+    }
+    ~MexFunction() = default;
+    void operator()(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs)
+    {
+
         if (inputs.size() < 2)
-            displayError("A,b have to be supplied");
-        
-        if (!isnumeric(inputs[0]) || !isnumeric(inputs[1]))
-            displayError("Data has to be numeric.");
+            utilities::error("A,b have to be supplied");
+
+        if (!utilities::isnumeric(inputs[0]) || !utilities::isnumeric(inputs[1]))
+            utilities::error("Data has to be numeric.");
 
         Polytope poly;
 
-        if (inputs.size() > 2) {
+        if (inputs.size() > 2)
+        {
             if (inputs.size() < 4)
-                displayError("If Aineq and Aeq are provided four arguments are required");
-            
+                utilities::error("If Aineq and Aeq are provided four arguments are required");
+
             if (inputs[0].getDimensions()[1] != inputs[2].getDimensions()[1])
-                displayError("Aineq and Aeq have to be in the same dimension");
-            
-            poly.setHrepEq(fromMatrixXd(convert(inputs[2])), fromMatrixXd(convert(inputs[3])));
+                utilities::error("Aineq and Aeq have to be in the same dimension");
+
+            poly.setHrepEq(fromMatrixXd(utilities::eigen::convert(inputs[2])), fromMatrixXd(utilities::eigen::convert(inputs[3])));
         }
-        
 
-        poly.setHrepIneq(fromMatrixXd(convert(inputs[0])), fromMatrixXd(convert(inputs[1])));
+        poly.setHrepIneq(fromMatrixXd(utilities::eigen::convert(inputs[0])), fromMatrixXd(utilities::eigen::convert(inputs[1])));
 
-        if (outputs.size()>0)
-            outputs[0] = convert(factory, fromMatrixXq(poly.getVertices()));
+        if (outputs.size() > 0)
+            outputs[0] = utilities::eigen::convert(fromMatrixXq(poly.getVertices()));
 
-        if (outputs.size()>1)
-            outputs[1] = convert(factory, fromMatrixXq(poly.getRays()));
-        
-        if (outputs.size()>2)
+        if (outputs.size() > 1)
+            outputs[1] = utilities::eigen::convert(fromMatrixXq(poly.getRays()));
+
+        if (outputs.size() > 2)
+        {
+            matlab::data::ArrayFactory factory;
             outputs[2] = factory.createScalar<double>(poly.getVolume());
-
+        }
     }
 };

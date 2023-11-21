6058c4aed206124181603289de196bb09ca0803d
Manuel Schaich <manuel.schaich@uk.alpineracing.com>
2023-11-21 21:12:30 +0000

    Build works on windows!

---
+++ lrslib/src/libgen-mockup.cpp
@@ -0,0 +1,36 @@
+#include <string>
+
+extern "C" {
+    char *basename(char *path);
+    char *dirname(char *path);
+}
+
+std::string
+pathname_directory(const std::string &pathname)
+{
+    std::size_t len = pathname.find_last_of("/\\");
+    return len == std::string::npos ? "": pathname.substr(0,len);
+}
+
+std::string
+pathname_sans_directory(const std::string &pathname)
+{
+    std::size_t len = pathname.find_last_of("/\\");
+    return len == std::string::npos ? pathname : pathname.substr(len + 1);
+}
+
+char *basename(char *path){
+    std::string pathname(path);
+    std::string basename = pathname_sans_directory(pathname);
+    char *cstr = new char[basename.length() + 1];
+    std::strcpy(cstr, basename.c_str());
+    return cstr;
+}
+
+char *dirname(char *path){
+    std::string pathname(path);
+    std::string dirname = pathname_directory(pathname);
+    char *cstr = new char[dirname.length() + 1];
+    std::strcpy(cstr, dirname.c_str());
+    return cstr;
+}

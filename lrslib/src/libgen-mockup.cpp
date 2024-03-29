33a75008b0728b2ed89e5c9fe9bdbf193454e423
Manuel Schaich <rmschaich@outlook.com>
2024-03-29 20:25:16 +0000

    Remove some warnings

--- lrslib/src/libgen-mockup.cpp
+++ lrslib/src/libgen-mockup.cpp
@@ -9,7 +9,7 @@ std::string
 pathname_directory(const std::string &pathname)
 {
     std::size_t len = pathname.find_last_of("/\\");
-    return len == std::string::npos ? "": pathname.substr(0,len);
+    return len == std::string::npos ? "" : pathname.substr(0,len);
 }
 
 std::string
@@ -23,7 +23,7 @@ char *basename(char *path){
     std::string pathname(path);
     std::string basename = pathname_sans_directory(pathname);
     char *cstr = new char[basename.length() + 1];
-    std::strcpy(cstr, basename.c_str());
+    strcpy_s(cstr, basename.length() + 1,basename.c_str());
     return cstr;
 }
 
@@ -31,6 +31,6 @@ char *dirname(char *path){
     std::string pathname(path);
     std::string dirname = pathname_directory(pathname);
     char *cstr = new char[dirname.length() + 1];
-    std::strcpy(cstr, dirname.c_str());
+    strcpy_s(cstr, dirname.length() + 1, dirname.c_str());
     return cstr;
 }
